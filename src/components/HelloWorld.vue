53e693fc1f746c6afbba41001859fca03648c65c
Olivier D'Hallewin <olivier.dhallewin@gmail.com>
2018-03-28 23:18:42 +0200

    Customize home component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,14 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <img src="@/assets/colibri.jpg" alt="Colibri" height="300px" class="mb-5">
         <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
+          &#8220;Colibri ! Tu n’es pas fou ? Ce n’est pas avec ces gouttes d’eau que tu vas éteindre le feu !&#8221;
+<br>
+Et le colibri lui répondit : &#8220;Je le sais, mais je fais ma part.&#8221;
           <footer>
             <small>
-              <em>&mdash;John Johnson</em>
+              <em>&mdash;Extrait d'une légende amérindienne</em>
             </small>
           </footer>
         </blockquote>
